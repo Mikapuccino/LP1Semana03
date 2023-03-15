58e6bc768bc40f2452fb6856e95c191c194fca6b
Mikapuccino <nelsonfcmilheiro@gmail.com>
2023-03-15 15:32:45 +0000

    Declared enumaration flag in Perks, program counts number of characters in given string

--- PlayerPerks/Perks.cs
+++ PlayerPerks/Perks.cs
@@ -1,10 +1,13 @@
+using System;
+
 namespace PlayerPerks
 {
+    [Flags]
     public enum Perks
     {
-        WaterBreathing ,
-        Stealth ,
-        AutoHeal ,
-        DoubleJump
+        WaterBreathing = 1 << 0,
+        Stealth        = 1 << 1,
+        AutoHeal       = 1 << 2,
+        DoubleJump     = 1 << 3
     };
 }
--- PlayerPerks/Program.cs
+++ PlayerPerks/Program.cs
@@ -6,7 +6,41 @@ namespace PlayerPerks
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Initialize variables used to count characters in the string
+            int wCount = 0;
+            int aCount = 0;
+            int sCount = 0;
+            int dCount = 0;
+            
+            // For each character in the string, it checks if it is "w", "a",
+            // "s" or "d", and increments appropriate variable
+            foreach (char c in args[0])
+            {                
+                if (c == 'w')
+                {
+                    wCount += 1;
+                }
+
+                else if (c == 'a')
+                {
+                    aCount += 1;
+                }
+
+                else if (c == 's')
+                {
+                    sCount += 1;
+                }
+
+                else if (c == 'd')
+                {
+                    dCount += 1;
+                }
+            }
+
+            Console.WriteLine(wCount);
+            Console.WriteLine(aCount);
+            Console.WriteLine(sCount);
+            Console.WriteLine(dCount);
         }
     }
 }
