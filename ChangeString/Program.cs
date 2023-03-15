f64d1058e208762498941f093bda8ef314629b4c
Mikapuccino <nelsonfcmilheiro@gmail.com>
2023-03-15 12:54:59 +0000

    Code structure done, need to fix errors

--- ChangeString/Program.cs
+++ ChangeString/Program.cs
@@ -6,11 +6,27 @@ namespace ChangeString
     {
         static void Main(string[] args)
         {
+            // Asks user for a string
             Console.WriteLine("Input a string: ");
             string s = Console.ReadLine();
 
+            // Asks user for a character
             Console.WriteLine("Input a character: ");
             char c = Console.ReadLine();
+
+            string newS;
+
+            // Checks each character in string s
+            foreach (char check in s)
+            {
+                // If character is different than char c, adds the
+                // character to string newS
+                if (c =! check);
+                newS += c;
+            }
+
+            // Writes the new string without the character the user specified
+            console.WriteLine(newS);
         }
     }
 }
