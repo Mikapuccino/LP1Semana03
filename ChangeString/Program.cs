9ca2d3dde7519e59432f6e15da970778f2557819
Mikapuccino <nelsonfcmilheiro@gmail.com>
2023-03-15 12:46:36 +0000

    Ask user for string and character

---
+++ ChangeString/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ChangeString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a string: ");
+            string s = Console.ReadLine();
+
+            Console.WriteLine("Input a character: ");
+            char c = Console.ReadLine();
+        }
+    }
+}
