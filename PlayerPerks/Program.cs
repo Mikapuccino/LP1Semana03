2537502a0adedcc211e00443f352234e47659f0e
Mikapuccino <nelsonfcmilheiro@gmail.com>
2023-03-15 15:53:17 +0000

    Perks appear properly based on characters in the first argument

--- PlayerPerks/Program.cs
+++ PlayerPerks/Program.cs
@@ -12,8 +12,8 @@ namespace PlayerPerks
             int sCount = 0;
             int dCount = 0;
             
-            // For each character in the string, it checks if it is "w", "a",
-            // "s" or "d", and increments appropriate variable
+            // For each character in the string, it checks if it is 'w', 'a',
+            // 's' or 'd', and increments appropriate variable
             foreach (char c in args[0])
             {                
                 if (c == 'w')
@@ -37,10 +37,33 @@ namespace PlayerPerks
                 }
             }
 
-            Console.WriteLine(wCount);
-            Console.WriteLine(aCount);
-            Console.WriteLine(sCount);
-            Console.WriteLine(dCount);
+            // Initialize userPerks with every possible perk
+            Perks userPerks = Perks.WaterBreathing | Perks.AutoHeal | 
+            Perks.Stealth | Perks.DoubleJump;
+            
+            // Removes perks based on how many specific characters
+            // the user input
+            if ((wCount % 2) == 0)
+            {
+                userPerks &= ~Perks.WaterBreathing;
+            }
+
+            if ((aCount % 2) == 0)
+            {
+                userPerks &= ~Perks.AutoHeal;
+            }
+
+            if ((sCount % 2) == 0)
+            {
+                userPerks &= ~Perks.Stealth;
+            }
+
+            if ((dCount % 2) == 0)
+            {
+                userPerks &= ~Perks.DoubleJump;
+            }
+
+            Console.WriteLine(userPerks);
         }
     }
 }
